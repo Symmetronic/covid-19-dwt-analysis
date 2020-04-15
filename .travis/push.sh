37bdd96f838721c0d879b0561693f29eb553a5ad
Simon Hochholzer <simon@symmetronic.com>
2020-04-15 18:46:25 +0200

    update script

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # TODO: Check Travis setup
-setup_git() {
+config() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
-commit_data_file() {
+commit() {
   git add src/assets/data/data.json
   git commit --message "Autoupdate data"
 }
 
 push() {
-  git remote add origin https://${GH_TOKEN}@github.com/Symmetronic/covid-19-dwt-analysis.git >/dev/null 2>&1
-  git push --quiet origin master >/dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/Symmetronic/covid-19-dwt-analysis.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master > /dev/null 2>&1
 }
 
-setup_git
-commit_data_file
+config
+commit
 push
