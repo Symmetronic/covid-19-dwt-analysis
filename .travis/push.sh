83b60e05dd5c1f6bf0f94dfa24114df7fe317081
Simon Hochholzer <simon@symmetronic.com>
2020-04-15 19:07:30 +0200

    update script

--- .travis/push.sh
+++ .travis/push.sh
@@ -12,7 +12,8 @@ commit() {
 }
 
 push() {
-  git push -f -q https://${GH_TOKEN}@github.com/Symmetronic/covid-19-dwt-analysis.git master >/dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/Symmetronic/covid-19-dwt-analysis.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master > /dev/null 2>&1
 }
 
 config
