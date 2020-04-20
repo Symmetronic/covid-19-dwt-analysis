ea980bc00d98083ff92d0938f2fd2d44341a9492
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-20 13:13:43 +0000

    Deploy Symmetronic/covid-19-dwt-analysis to github.com/Symmetronic/covid-19-dwt-analysis.git:gh-pages

--- build/app.js
+++ build/app.js
@@ -125,7 +125,7 @@ var StyleNode=function(){this.start=0,this.end=0,this.previous=null,this.parent=
 
   var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
   var start = function() {
-    var url = new URL('./p-c8364a36.system.js', resourcesUrl);
+    var url = new URL('./p-05c9b5c3.system.js', resourcesUrl);
     System.import(url.href);
   };
 
